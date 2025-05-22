2880183bb35739d6994c3947910d45a9fe0296f4
malakhatem1 <malakhd.2003@gmail.com>
2025-05-22 11:38:04 +0300

    second smell(Handle empty step method for financial data request with TODO and exception)

--- src/test/java/com/MyCooking/steps/BillingSteps.java
+++ src/test/java/com/MyCooking/steps/BillingSteps.java
@@ -2,6 +2,10 @@ package com.MyCooking.steps;
 
 import io.cucumber.java.en.*;
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import MyCooking.com.BillingSystem;
 import MyCooking.com.models.*;
 
@@ -33,7 +37,13 @@ public class BillingSteps {
 
     @When("financial data is requested")
     public void requestFinancialData() {
-        
+    	List<Invoice> invoices = new ArrayList<>();
+        invoices.add(new Invoice("User001", 100.0));
+        invoices.add(new Invoice("User002", 200.0));
+        invoices.add(new Invoice("User003", 150.0));
+
+        FinancialReport report = new FinancialReport(invoices);
+        report.displayReport();  
     }
 
     @Then("the system should display up-to-date financial reports")
@@ -43,53 +53,3 @@ public class BillingSteps {
     }
 }
 
-
-
-
-
-
-/*
-package com.MyCooking.steps;
-
-import io.cucumber.java.en.*;
-import static org.junit.Assert.*;
-import MyCooking.com.BillingSystem;
-import MyCooking.com.models.*;
-
-public class BillingSteps {
-
-    private BillingSystem  BillingSystem = new BillingSystem();
-
-    @Given("the customer completes an order")
-    public void completeOrder() {
-    	 BillingSystem.completeOrder();
-    }
-
-    @When("the order is finalized")
-    public void finalizeOrder() {
-    	BillingSystem.finalizeOrder();
-    }
-
-    @Then("the system should generate and send an invoice")
-    public void generateInvoice() {
-    	BillingSystem.generateAndSendInvoice("Customer01");
-        assertTrue(!BillingSystem.getInvoices().isEmpty());
-    }
-
-    @Given("the administrator logs into the system")
-    public void adminLogin() {
-    	BillingSystem.adminLogin();
-    }
-
-    @When("financial data is requested")
-    public void requestFinancialData() {
-    
-    }
-
-    @Then("the system should display up-to-date financial reports")
-    public void displayFinancialReports() {
-    	BillingSystem.displayFinancialReport();
-        assertTrue(true);
-    }
-}
-*/
